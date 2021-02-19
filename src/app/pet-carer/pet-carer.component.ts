098bc51fb2c668a8ca5c76cd379341cdcd876796
Thalis Vicente <thalisvicente@gmail.com>
2021-02-18 22:03:36 -0500

    added Pet Carer register form

---
+++ src/app/pet-carer/pet-carer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-carer',
+  templateUrl: './pet-carer.component.html',
+  styleUrls: ['./pet-carer.component.css']
+})
+export class PetCarerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
