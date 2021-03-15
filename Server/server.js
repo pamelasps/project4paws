b3ed1e61106a809e98d26e6b63884502cbe31e04
Thalis Vicente <thalisvicente@gmail.com>
2021-03-15 07:15:49 -0400

    Update server.js file. Please adjust the DB name, host and password when working on your local repository

--- Server/server.js
+++ Server/server.js
@@ -1,112 +1,92 @@
+//Setting dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
-//const cors = require('cors');
+const cors = require('cors');
 const mysql = require('mysql');
-
-
 const PORT = 3000;
+
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
+
+// parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-//app.use(cors());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
 
+//Test connection from server response
+app.get('/', function (req, res) { 
+    res.json({ message: "Welcome to 4Paws application." });
+})
+
+//Console current server localhost port connection
+app.listen(PORT, function () {
+    console.log("Server running on localhost: " + PORT);
+});
+
+//Database creation and connection
 let db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: '4paws'
+    database: '4pawsv6'
 
 });
 
-db.connect(function(err) {
+module.exports = db;
+
+db.connect(function (err) {
     if (err) throw err;
     console.log("Database Connected!");
-  });
-  module.exports = db;
-
-app.get('/', function (req, res) { //from server
-    res.send('Hello from server');
-})
-
-app.post('/enroll', function (req, res) { //towards server
-    //console.log(req.body);
-    let postBody = req.body;
-    console.log(postBody);
-   
-    let firstName = postBody.firstName;
-    let lastName = postBody.lastName
-    let email = postBody.email;
-    let city = postBody.city;
-    let province = postBody.province;
-    let postcode = postBody.postcode;
-    let country = postBody.country;
-    let phone = postBody.phone;
-    let dob = postBody.dob;
-
-    let photo = postBody.photo;
-    let breedPref = postBody.breedPref;
-    let carerNeighbor = postBody.carerNeighbor;
-    let aboutCarer = postBody.aboutCarer;
-    let bgcheck = postBody.bgcheck;
-    let hPrice = postBody.hPrice;
-
-
-        db.query('INSERT INTO sitter (sitter_firstname, sitter_lastname, sitter_email, sitter_phone, sitter_dob, sitter_photo, sitter_policeCheck, sitter_price) VALUES (?,?,?,?,?,?,?,?)', [firstName, lastName, email, phone, dob, photo, bgcheck, hPrice],
-        (err, res) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('Registered!');
-            }
-
-        });
-        
-
-    res.status(200).send({ "message": "Data Received" });
-   
 });
 
-// Pet Owner Register
-
-app.post('/petOwner', function (req, res) { //towards server
-    //console.log(req.body);
+//Insert PetCarer registration into SITTER table in 4Paws database
+//Towards server
+app.post('/enroll', function (req, res) { 
     let postBody = req.body;
+    //Console on Server terminal the body request from client
     console.log(postBody);
-
-    let sql = "INSERT INTO owner SET ?";
+    //SQL query to be executed
+    let sql = "INSERT INTO sitter SET ?";
     let postQuery = db.query(sql, postBody, (err, results) => {
         if (err) throw err;
         console.log('Registered!');
+        //Message send to Client console as response for data received
+       
         res.status(200).end(JSON.stringify(results.insertId));
-    })
-   
-    // let firstName = postBody.firstName;
-    // let lastName = postBody.lastName
-    // let email = postBody.email;
-    // let phone = postBody.phone;
-    // let city = postBody.city;
-    // let province = postBody.province;
-    // let country = postBody.country;
-    // let postalcode = postBody.postcode;
+        //let id = JSON.stringify(results.insertId);
+        //console.log(id);
 
-    //     db.query('INSERT INTO owner (owner_firstname, owner_lastname, owner_email, owner_phone, owner_address, owner_city, owner_province, owner_country, owner_postalCode) VALUES (?,?,?,?,?,?,?,?,?)', [firstName, lastName, email, phone, city, province, country, postalcode],
-    //     (err, res) => {
-    //         if (err) {
-    //             console.log(err);
-    //         } else {
-    //             console.log('Registered!');
-    //         }
+    });
+    
 
-    //     });
-        
-
-    // res.status(200).send({ "message": "Data Received" });
-   
 });
 
+//Get PetCarer info from SITTER table in 4Paws database
+//Towards server
 
-app.listen(PORT, function () {
-    console.log("Server running on localhost: " + PORT);
+/*
+app.get('/carerProfile', (req, res) => { 
+    //console.log(req.params);
+    let sql = 'SELECT * FROM sitter';
+    let query = db.query(sql, (err, results) => {
+        if (err) throw err;
+        console.log(results);
+        res.send(results);
+    });
+
+});
+*/
+
+app.get('/carerProfile/:id', (req, res) => { 
+    let requestedID = req.params.id;
+    let sql = 'SELECT * FROM sitter where sitter_id = ?';
+    let query = db.query(sql, requestedID, (err, results) => {
+        if (err) throw err;
+        console.log(results);
+        //res.status(200).send(JSON.stringify(results));
+        res.send(results);
+    });
+
 });
